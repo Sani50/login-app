22101e44f9c3412f5b50e884c0707db51d140a35
Sanvi <sanvimisra1358@mail.com>
2026-03-05 12:29:33 +0530

    login app

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Backend is connected!" });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin" && password === "admin") {
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port 5000");
+});
